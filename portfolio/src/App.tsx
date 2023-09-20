38363cd41f0546c0ef8faa6a2c666b4f9e8615c5
Bravo-Luis <luisbravo@ucsb.edu>
2023-09-20 02:01:03 -0700

    firebase

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -6,33 +6,56 @@ import { createTheme } from '@mui/material/styles';
 
 function App() {
 
-
+  const defaultTheme = createTheme();
   const theme = createTheme({
     palette: {
-      primary: {
-        main: '#d7d5c4',  
-      },
-      secondary: {
-        main: '#50C878',  
-      },
+        primary: {
+            main: '#d7d5c4',  
+        },
+        secondary: {
+            main: '#50C878',  
+        },
     },
     typography: {
-      h1: {
-        fontFamily: 'Montserrat',
-        fontWeight: 500,
-        fontSize: "10rem",
-        lineHeight: '6rem',
-      },
-      h2: {
-        fontFamily: 'Montserrat',
-        fontWeight: 500,
-        fontSize: "4rem",
-        lineHeight: '2rem',
-      }
-
+        h1: {
+            fontFamily: 'Montserrat',
+            fontWeight: 500,
+            fontSize: "10rem",
+            lineHeight: '8rem',
+            [defaultTheme.breakpoints.down('md')]: {
+                fontSize: "7rem",
+                lineHeight: '6rem',
+            },
+            [defaultTheme.breakpoints.down('sm')]: {
+                fontSize: "4rem",
+                lineHeight: '2rem',
+            },
+            [defaultTheme.breakpoints.down('xs')]: {
+                fontSize: "3rem",
+                lineHeight: '2.5rem',
+            }
+        },
+        h2: {
+            fontFamily: 'Montserrat',
+            fontWeight: 500,
+            fontSize: "4rem",
+            lineHeight: '5rem',
+            [defaultTheme.breakpoints.down('md')]: {
+                fontSize: "3rem",
+                lineHeight: '4rem',
+            },
+            [defaultTheme.breakpoints.down('sm')]: {
+                fontSize: "2rem",
+                lineHeight: '5rem',
+            },
+            [defaultTheme.breakpoints.down('xs')]: {
+                fontSize: "1.5rem",
+                lineHeight: '0rem',
+            }
+        }
     }
-    
-  });
+});
+
 
 
   return (
--- portfolio/src/Header/Header.tsx
+++ portfolio/src/Header/Header.tsx
@@ -1,15 +1,42 @@
 import './Header.css'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import Button from '@mui/material/Button';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
 import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 function Header(){
 
- useEffect(() => {
+    const [drawerOpen, setDrawerOpen] = useState(false);
 
- }, [])
 
- 
+    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+        if (
+          event &&
+          event.type === 'keydown' &&
+          ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
+        ) {
+          return;
+        }
+        setDrawerOpen(open);
+      };
+
+    const drawerList = () => (
+        <div
+          role="presentation"
+          onClick={toggleDrawer(false)}
+          onKeyDown={toggleDrawer(false)}
+        >
+          <div className="header-menu-item"> About </div>
+          <Divider />
+          <div className="header-menu-item"> Work Experience </div>
+          <Divider />
+          <div className="header-menu-item"> Projects </div>
+          <Divider />
+          <div className="header-menu-item"> Resume </div>
+          <Divider />
+        </div>
+      );
 
     return(
         <div className="header">
@@ -18,6 +45,17 @@ function Header(){
                 <Button variant="outlined"> Work Experience </Button>
                 <Button variant="outlined"> Projects </Button>
                 <Button variant="contained"> Resume</Button>
+            </div>
+            <div className="header-menu">
+            <Button id='hamburger' onClick={toggleDrawer(true)}>☰</Button>
+            <SwipeableDrawer
+                anchor='top'
+                open={drawerOpen}
+                onClose={toggleDrawer(false)}
+                onOpen={toggleDrawer(true)}
+            >
+                {drawerList()}
+            </SwipeableDrawer>
             </div>
             <div className="header-jobtitle">
             <Typography variant="h2"> Software Engineer </Typography> 
