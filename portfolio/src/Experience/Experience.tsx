edf19045898df7ce12703dd08f61c4ba195bd1b7
Bravo-Luis <luisbravo@ucsb.edu>
2023-09-28 13:01:47 -0700

    experience page and boxes

--- portfolio/src/Experience/Experience.tsx
+++ portfolio/src/Experience/Experience.tsx
@@ -6,7 +6,7 @@ function Experience(){
     return (
         <div className='experience-page'>
             <ExperienceBox title='Salesforce' jobTitle='FTL Scholar' dates="Summer 2023" bgColor='#00A1E0'/>
-            <ExperienceBox title='Salesforce' jobTitle='Software Engineer Intern' dates="Incoming Summer 2024" bgColor='#00A1E0'/>
+            <ExperienceBox title='Salesforce' jobTitle='Software Engineer Intern' dates="Return Offer Summer 2024" bgColor='#00A1E0'/>
         </div>
     )
 }
